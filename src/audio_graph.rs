eac2340045b13adcf781642e6dab55553bd79c7e
Dan Tong <dan@dtong.nz>
2023-05-15 17:18:56 +1200

    Use rain.wav as default sample

--- src/audio_graph.rs
+++ src/audio_graph.rs
@@ -1,10 +1,8 @@
 use std::{fs::File, io};
-use web_audio_api::node::{AudioNode, AudioScheduledSourceNode};
-use web_audio_api::{
-    context::{AudioContext, BaseAudioContext},
-    node::GainNode,
-};
+use web_audio_api::context::{AudioContext, BaseAudioContext};
+use web_audio_api::node::{AudioNode, AudioScheduledSourceNode, BiquadFilterNode, GainNode};
 
+const SAMPLE_FILE: &str = "samples/rain.wav";
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("failed to open file")]
@@ -20,14 +18,11 @@ pub struct AudioGraph {
 
 impl AudioGraph {
     pub fn new() -> Result<Self, Error> {
-        // set up the audio context with optimized settings for your hardware
         let context = AudioContext::default();
 
-        // for background music, read from local file
-        let file = File::open("samples/major-scale.ogg")?;
+        let file = File::open(SAMPLE_FILE)?;
         let buffer = context.decode_audio_data_sync(file)?;
 
-        // create gain control
         let volume = context.create_gain();
         volume.gain().set_value(0.5);
 
@@ -45,14 +40,12 @@ impl AudioGraph {
             .frequency()
             .set_value_at_time(1800.0, context.current_time());
 
-        // setup an AudioBufferSourceNode
         let src = context.create_buffer_source();
         src.set_buffer(buffer);
         src.set_loop(true);
         // TODO: get playback rate working
         // src.playback_rate().set_value(0.1);
 
-        // pipe it all together
         src.connect(&lp_filter);
         lp_filter.connect(&hp_filter);
         hp_filter.connect(&volume);
